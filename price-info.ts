371d7b828782f259d36930782ff1b7b5da08ea33
Kiyanoosh Khosravi <Kiyanoosh.Khosravi@certegy.com.au>
2017-02-09 09:18:45 +1030

    XPAY-988
    The lower price of the instalments values is now displayed as main
    payment value

--- price-info.ts
+++ price-info.ts
@@ -4,28 +4,28 @@ import * as jq from 'jquery';
 
 (($: JQueryStatic) => {
 
-
     var productPrice: number;
     var scriptTag = document.getElementsByTagName('script');
     var scriptIndex  = scriptTag[scriptTag.length - 1];
 
     var queryProductPrice = scriptIndex.src.replace(/^[^\?]+\??/, '').substring(13);
 
-    productPrice = parseInt(queryProductPrice);
+    productPrice = parseFloat(queryProductPrice);
 
     if (!productPrice) {
         productPrice = 0;
     }
 
-    var dividedProductPriceByFour = productPrice / 4;
-    var roundedProductPrice = dividedProductPriceByFour.toFixed(2);
+    var productPriceDividedByFour = productPrice / 4;
+    var roundedDownProductPrice = Math.floor( productPriceDividedByFour * Math.pow(10, 2) ) / Math.pow(10, 2) ;
 
     const template = `<a id="oxipay-tag-02" href="#${Config.infoModalId}">
-            <p>or only 4 payments of $${roundedProductPrice} by</p> <img alt="Oxipay" src="${Config.baseContentUrl}/content/images/oxipay.svg" /> <span>More info</span>
+            <p>or only 4 payments of $${roundedDownProductPrice} by</p> <img alt="Oxipay" src="${Config.baseContentUrl}/content/images/oxipay.svg" /> <span>More info</span>
         </a>`;
 
     const widget = new ModalInjector($);
     widget.injectBanner(template, Config.moreInfoUrl);
+
+
+
 })(jq);
-
-
