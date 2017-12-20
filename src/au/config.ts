f6cacb97dc3c1d0c2db4a7a526cc46e026fc273e
Tao Wang <tao.wang@certegy.com.au>
2017-12-20 17:01:20 +1030

    now the Australian part configured to serve from widgets.oxipay.com.au

--- src/au/config.ts
+++ src/au/config.ts
@@ -1,11 +1,11 @@
 ﻿// when doing local test, set:
-//   baseContentUrl = http://localhost:port_number
+//   baseContentUrl = http://localhost:PORT_NUMBER/dist/au' (or 'http://localhost:PORT_NUMBER/dist/nz' for New Zealand)
 
 // When deploying to server:
-//   baseContentUrl = 'https://YOUR-URL/dist/au' (or 'https://YOUR-URL/dist/nz' for New Zealand)
+//   baseContentUrl = 'https://YOUR-URL'
 
 export class Config {
-    public static baseContentUrl = 'http://widgets.oxipay.co.nz/dist';    // for remote deploy
+    public static baseContentUrl = 'http://widgets.oxipay.com.au';    // for remote deploy
     // public static baseContentUrl = 'http://localhost:3000/dist/au';       // for local testing
     public static oxipayModalId = 'oxipay-modal';
     public static signupModalId = 'oxipay-modal-signup';
